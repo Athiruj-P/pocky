a3c01e54c41c46e25edfd2663418bc0c53d8a642
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2020-02-27 20:25:27 +0700

    Create account class

---
+++ ionic-Pocky/src/app/pattern.component.ts
@@ -0,0 +1,27 @@
+
+export class Account {
+    private username:string;
+    private password:string;
+    // private wallet:Wallet[];
+    constructor(){}
+
+    setUsername(username){
+        this.username = username;
+    }
+
+    getUsername(){
+        return this.username;
+    }
+
+    setPassword(password){
+        this.password = password;
+    }
+
+    login(){
+        // 
+    }
+
+    reginter(){
+        // 
+    }
+}
