6c89a173f46e9485f1fbd0f131e1d1139aaa0a1b
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2020-02-27 20:37:26 +0700

    edit

--- ionic-Pocky/src/app/login/login.page.ts
+++ ionic-Pocky/src/app/login/login.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+/**
+ * Import classes from pattern.component.ts
+ * เข้าถึงคลาสต่าง ๆ ที่ต้องใช้ โดยกำหนดชื่อของคลาสที่ต้องการ
+ * Author: Athiruj Poositaporn
+ * Create date: 27/02/2020
+ */
+import { Account } from '../pattern.component';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor(private router: Router) { }
+  private uername:string;
+  private password:string;
+  constructor(private router: Router,private Account: Router) { }
 
   ngOnInit() {
   }
