74164ba051d8fdb9d43a51b8d95ca6b61ca6ffa3
Saharat Pinyoprutepong <60160181@go.buu.ac.th>
2020-03-10 18:42:23 +0700

    edit path

--- ionic-Pocky/src/app/app-routing.module.ts
+++ ionic-Pocky/src/app/app-routing.module.ts
@@ -2,21 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'wallet-detail', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'wallet-detail',
-    loadChildren: () => import('./wallet-detail/wallet-detail.module').then( m => m.WalletDetailPageModule)
-  },
-  {
-    path: 'add-transaction',
-    loadChildren: () => import('./add-transaction/add-transaction.module').then( m => m.AddTransactionPageModule)
-  },
+  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule) },
+  { path: 'add-new-wallet', loadChildren: () => import('./add-new-wallet/add-new-wallet.module').then(m => m.AddNewWalletPageModule) },
+  { path: 'wallet-detail', loadChildren: () => import('./wallet-detail/wallet-detail.module').then(m => m.WalletDetailPageModule) },
+  { path: 'add-transaction', loadChildren: () => import('./add-transaction/add-transaction.module').then(m => m.AddTransactionPageModule) },
+  { path: 'edit-transaction', loadChildren: () => import('./edit-transaction/edit-transaction.module').then(m => m.EditTransactionPageModule) }
 
 ];
 
