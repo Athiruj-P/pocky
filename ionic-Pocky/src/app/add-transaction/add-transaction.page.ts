6459a1ccfbdb68e7cec80f7d65685a4ff53cc9d0
Saharat Pinyoprutepong <60160181@go.buu.ac.th>
2020-03-10 19:56:42 +0700

    development add transaction page

--- ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
+++ ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Account } from '../pattern.component';
+import { Income, Expenditure } from '../pattern.component';
 
 @Component({
   selector: 'app-add-transaction',
@@ -7,13 +9,43 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./add-transaction.page.scss'],
 })
 export class AddTransactionPage implements OnInit {
+  index;
+  public segment = "Expenses";
+  private balance = 0.00;
+  private note = "";
 
-  constructor(public modalController:ModalController) { }
+  constructor(public modalController: ModalController, private account: Account) { }
 
   ngOnInit() {
   }
 
-  async closeModal(){
+  addTransaction() {
+    if (this.segment == "Income") {
+      var type = 1;
+      var tmp_tran = new Income;
+    } else {
+      var type = 2;
+      var tmp_tran = new Expenditure;
+    }
+    tmp_tran.setDescription(this.note);
+    tmp_tran.setAmount(this.balance);
+    var json = {
+      "tran_name": this.note,
+      "tran_amount": this.balance,
+      "tran_wal_id": this.account.getWallet()[this.index].getId(),
+      "tran_status": "Y",
+      "tran_type": type
+    }
+    console.log(json);
+    this.account.databaseService.add_transaction(json).subscribe((res) => {
+      tmp_tran.setTransactionId(res[0].tran_id);
+      tmp_tran.setDateTime(res[0].tran_date);
+      this.account.getWallet()[this.index].addTransaction(tmp_tran);
+      console.log(this.account.getWallet()[this.index]);
+      this.closeModal();
+    });
+  }
+  async closeModal() {
     await this.modalController.dismiss();
   }
 }
