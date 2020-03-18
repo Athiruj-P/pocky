e62161108c73b618e5cf6a994651fbbb59ba9a16
Saharat Pinyoprutepong <60160181@go.buu.ac.th>
2020-03-18 12:12:03 +0700

    Modify add transaction page

--- ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
+++ ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
@@ -20,30 +20,37 @@ export class AddTransactionPage implements OnInit {
   }
 
   addTransaction() {
-    if (this.segment == "Income") {
-      var type = 1;
-      var tmp_tran = new Income;
-    } else {
-      var type = 2;
-      var tmp_tran = new Expenditure;
-    }
-    tmp_tran.setDescription(this.note);
-    tmp_tran.setAmount(this.balance);
-    var json = {
-      "tran_name": this.note,
-      "tran_amount": this.balance,
-      "tran_wal_id": this.account.getWallet()[this.index].getId(),
-      "tran_status": "Y",
-      "tran_type": type
-    }
-    console.log(json);
-    this.account.databaseService.add_transaction(json).subscribe((res) => {
-      tmp_tran.setTransactionId(res[0].tran_id);
-      tmp_tran.setDateTime(res[0].tran_date);
-      this.account.getWallet()[this.index].addTransaction(tmp_tran);
-      console.log(this.account.getWallet()[this.index]);
+    if (this.balance <= 0) {
+      this.showToast(`Balance must greater than 0 !!`, "danger");
+    } else if (this.note && this.balance) {
+      if (this.segment == "Income") {
+        var type = 1;
+        var tmp_tran = new Income;
+      } else {
+        var type = 2;
+        var tmp_tran = new Expenditure;
+      }
+      tmp_tran.setDescription(this.note);
+      tmp_tran.setAmount(this.balance);
+      var json = {
+        "tran_name": this.note,
+        "tran_amount": this.balance,
+        "tran_wal_id": this.account.getWallet()[this.index].getId(),
+        "tran_status": "Y",
+        "tran_type": type
+      }
+      console.log(json);
+      this.account.databaseService.add_transaction(json).subscribe((res) => {
+        tmp_tran.setTransactionId(res[0].tran_id);
+        tmp_tran.setDateTime(res[0].tran_date);
+        this.account.getWallet()[this.index].addTransaction(tmp_tran);
+        console.log(this.account.getWallet()[this.index]);
+      this.showToast("Added a new transaction", "warning");
       this.closeModal();
-    });
+      });
+    } else {
+      this.showToast(`Please fill in all informations!!`, "danger");
+    }
   }
   async closeModal() {
     await this.modalController.dismiss();
