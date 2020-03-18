d9b2fdbf83f9e30e96216f8fb624d5882773284e
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2020-03-18 15:17:03 +0700

    edit function  in add-transaction

--- ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
+++ ionic-Pocky/src/app/add-transaction/add-transaction.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+
 import { Account } from '../pattern.component';
 import { Income, Expenditure } from '../pattern.component';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-add-transaction',
   templateUrl: './add-transaction.page.html',
@@ -14,38 +15,58 @@ export class AddTransactionPage implements OnInit {
   private balance = 0.00;
   private note = "";
 
-  constructor(public modalController: ModalController, private account: Account) { }
+  constructor(public toastController: ToastController, public modalController: ModalController, private account: Account) { }
 
   ngOnInit() {
+    console.log(this.index)
+  }
+
+  async showToast(mess, color) {
+    const toast = await this.toastController.create({
+      mode: "ios",
+      message: mess,
+      position: 'top',
+      duration: 1000,
+      color: color
+    });
+    toast.present();
   }
 
   addTransaction() {
-    if (this.segment == "Income") {
-      var type = 1;
-      var tmp_tran = new Income;
-    } else {
-      var type = 2;
-      var tmp_tran = new Expenditure;
-    }
-    tmp_tran.setDescription(this.note);
-    tmp_tran.setAmount(this.balance);
-    var json = {
-      "tran_name": this.note,
-      "tran_amount": this.balance,
-      "tran_wal_id": this.account.getWallet()[this.index].getId(),
-      "tran_status": "Y",
-      "tran_type": type
-    }
-    console.log(json);
-    this.account.databaseService.add_transaction(json).subscribe((res) => {
-      tmp_tran.setTransactionId(res[0].tran_id);
-      tmp_tran.setDateTime(res[0].tran_date);
-      this.account.getWallet()[this.index].addTransaction(tmp_tran);
-      console.log(this.account.getWallet()[this.index]);
+    if (this.balance <= 0) {
+      this.showToast(`Balance must greater than 0 !!`, "danger");
+    } else if (this.note && this.balance) {
+      if (this.segment == "Income") {
+        var type = 1;
+        var tmp_tran = new Income;
+      } else {
+        var type = 2;
+        var tmp_tran = new Expenditure;
+      }
+      tmp_tran.setDescription(this.note);
+      tmp_tran.setAmount(this.balance);
+      var json = {
+        "tran_name": this.note,
+        "tran_amount": this.balance,
+        "tran_wal_id": this.account.getWallet()[this.index].getId(),
+        "tran_status": "Y",
+        "tran_type": type
+      }
+      console.log(json);
+      this.account.databaseService.add_transaction(json).subscribe((res) => {
+        tmp_tran.setTransactionId(res[0].tran_id);
+        tmp_tran.setDateTime(res[0].tran_date);
+        this.account.getWallet()[this.index].addTransaction(tmp_tran);
+        console.log(this.account.getWallet()[this.index]);
+      this.showToast("Added a new transaction", "warning");
       this.closeModal();
-    });
+      });
+    } else {
+      this.showToast(`Please fill in all informations!!`, "danger");
+    }
   }
+
   async closeModal() {
-    await this.modalController.dismiss();
+    await this.modalController.dismiss(this.index);
   }
 }
